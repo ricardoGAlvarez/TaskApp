88f535a77d2fba06e5d42067bc2b91f5e58b264d
ricardoGAlvarez <ricardo.gabriel.alvarez94@gmail.com>
2023-05-31 20:06:46 -0300

    primer prueba

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
+from datetime import datetime
 
 app=Flask (__name__)
 mysql=MySQL()
@@ -11,19 +12,25 @@ app.config['MYSQL_DATABASE_PASSWORD']=''
 app.config['MYSQL_DATABASE_DB']='bdejemplopy'
 mysql.init_app(app)
 
+
 @app.route('/')
 def inicio():
     return render_template('pages/index.html')
 
 @app.route('/realizados')
 def realizados():
-    return render_template('pages/realizados.html')
+    conexion=mysql.connect()
+    cursor= conexion.cursor()
+    cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 1")
+    reclamo=cursor.fetchall()
+    conexion.commit()
+    return render_template('pages/realizados.html', reclamo=reclamo)
 
 @app.route('/pendientes')
 def pendientes():
     conexion=mysql.connect()
     cursor= conexion.cursor()
-    cursor.execute("SELECT * FROM `reclamos`")
+    cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 0")
     reclamo=cursor.fetchall()
     conexion.commit()
     return render_template('pages/pendientes.html', reclamo=reclamo)
@@ -39,9 +46,10 @@ def guardar():
     _calle=request.form['txtCalle']
     _barrio=request.form['txtBarrio']
     _problema=request.form['txtProblema']
+    _estado=0
     
-    sql= "INSERT INTO   `reclamos` ( `id`, `nombre`, `barrio`,  `calle`,  `problema`) VALUE(NULL,%s,%s,%s,%s);"
-    datos=(_nombre,_calle,_barrio,_problema )
+    sql= "INSERT INTO   `reclamos` ( `id`, `nombre`, `barrio`,  `calle`,  `problema`,`estado`) VALUE(NULL,%s,%s,%s,%s,%s);"
+    datos=(_nombre,_calle,_barrio,_problema,_estado )
     conexion= mysql.connect()
     cursor=conexion.cursor()
     cursor.execute(sql,datos)
@@ -61,7 +69,9 @@ def editar(id):
     calle = elemento[2]
     barrio = elemento[3]
     problema = elemento[4]
-    return render_template('pages/edit.html', id=id, nombre=nombre, calle=calle, barrio=barrio, problema=problema)
+    estado= elemento[5]
+
+    return render_template('pages/edit.html', id=id, nombre=nombre, calle=calle, barrio=barrio, problema=problema, estado=estado)
 
 @app.route('/edit/guardar/<int:id>', methods=['POST'])
 def actualizar(id):
@@ -69,8 +79,9 @@ def actualizar(id):
     _calle = request.form['txtCalle']
     _barrio = request.form['txtBarrio']
     _problema = request.form['txtProblema']
-    sql = "UPDATE `reclamos` SET `nombre` = %s, `barrio` = %s, `calle` = %s, `problema` = %s WHERE `reclamos`.`id` = %s"
-    datos = (_nombre, _barrio, _calle, _problema, id)
+    _estado= request.form['txtEstado']
+    sql = "UPDATE `reclamos` SET `nombre` = %s, `barrio` = %s, `calle` = %s, `problema` = %s, `estado` = %s WHERE `reclamos`.`id` = %s"
+    datos = (_nombre, _barrio, _calle, _problema,_estado, id)
     print(dir(datos))
     conexion = mysql.connect()
     cursor = conexion.cursor()
@@ -79,22 +90,54 @@ def actualizar(id):
     return redirect('/pendientes')
 
 @app.route('/realizados/guardar/<int:id>', methods=['POST'])
-def guardar_realizado(id):
+def realizado(id):
+    _estado= 1
+    sql = "UPDATE `reclamos` SET `estado` = %s WHERE `reclamos`.`id` = %s"
+    datos = (_estado, id)
+    print(dir(datos))
     conexion = mysql.connect()
     cursor = conexion.cursor()
-    sql = "SELECT * FROM reclamos WHERE id = %s"
-    cursor.execute(sql, (id,))
-    elemento = cursor.fetchone()
-    nombre = elemento[1]
-    calle = elemento[2]
-    barrio = elemento[3]
-    problema = elemento[4]
-    return render_template('pages/edit.html', id=id, nombre=nombre, calle=calle, barrio=barrio, problema=problema)
+    cursor.execute(sql, datos)
+    conexion.commit()
+    return redirect('/pendientes')
 
+@app.route('/realizados/pendientes/<int:id>', methods=['POST'])
+def ponerpendiente(id):
+    _estado= 0
+    sql = "UPDATE `reclamos` SET `estado` = %s WHERE `reclamos`.`id` = %s"
+    datos = (_estado, id)
+    print(dir(datos))
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute(sql, datos)
+    conexion.commit()
+    return redirect('/realizados')
 
+@app.route('/buscar/realizados')
+def buscarrealizados():
+    barrio = request.args.get('barrio')
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    if barrio:
+        cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 1 AND `barrio` = %s", (barrio,))
+    else:
+        cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 1")
+    reclamo = cursor.fetchall()
+    conexion.commit()
+    return render_template('pages/realizados.html', reclamo=reclamo)
 
-
-
+@app.route('/buscar/pendientes')
+def buscarpendientes():
+    barrio = request.args.get('barrio')
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    if barrio:
+        cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 0 AND `barrio` = %s", (barrio,))
+    else:
+        cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 0")
+    reclamo = cursor.fetchall()
+    conexion.commit()
+    return render_template('pages/pendientes.html', reclamo=reclamo)
 
 if __name__=='__main__':
     app.run(debug=True)
