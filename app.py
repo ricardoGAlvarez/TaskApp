156504c56c5891fd987cca09e790093beb0c1e65
ricardoGAlvarez <ricardo.gabriel.alvarez94@gmail.com>
2023-06-06 12:07:51 -0300

    ultima version

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask
+from flask import Flask,session 
 from flask import render_template, request, redirect
 from flaskext.mysql import MySQL
 from datetime import date
 from flask import flash
-from tkinter import messagebox 
+from tkinter import messagebox
 app=Flask (__name__)
 mysql=MySQL()
 
@@ -22,24 +22,39 @@ mysql.init_app(app)
 
 usuario="pepe"
 password="1234"
+app.secret_key = 'test'
+
+
+
 @app.route('/')
 def inicio():
     return render_template('pages/index.html')
 
+@app.before_request
+def bloquear_endpoints():
+    ruta_actual = request.endpoint
+    if  ruta_actual != 'inicio' and not request.path.startswith('/static'):
+        # Verificar si el usuario está autenticado
+        if 'usuario' in session:
+            nombre_usuario=session['usuario']
+            return redirect('/pendientes')
 
 @app.route('/login', methods=['POST'])
 def login():
     _usuario = request.form['txtUsuario']
     _password = request.form['txtPassword']
     if _usuario != usuario or _password != password:
-        return redirect('/')
+        return render_template('pages/index.html', error=True)
     else:
-        conexion=mysql.connect()
-        cursor= conexion.cursor()
+        # Almacenar el nombre del usuario en la sesión
+        session['usuario'] = _usuario
+        conexion = mysql.connect()
+        cursor = conexion.cursor()
         cursor.execute("SELECT * FROM `reclamos` WHERE `estado` = 0")
-        reclamo=cursor.fetchall()
+        reclamo = cursor.fetchall()
         conexion.commit()
-        return render_template('pages/pendientes.html', reclamo=reclamo)
+        return render_template('pages/pendientes.html', reclamo=reclamo, usuario=_usuario)
+
 
 @app.route('/realizados')
 def realizados():
@@ -71,7 +86,6 @@ def guardar():
     _barrio=request.form['txtBarrio']
     _problema=request.form['txtProblema']
     _estado=0
-    _historial=_problema
     _fecha= fecha_actual
     sql_filter="SELECT calle FROM `reclamos` WHERE calle=%s"
     conexion= mysql.connect()
@@ -82,8 +96,8 @@ def guardar():
        messagebox.showerror("Error", "La direccion ya posee un reclamo")
        return redirect('/nuevo')
     else:
-        sql= "INSERT INTO   `reclamos` ( `id`, `nombre`, `barrio`,  `calle`,  `problema`,`estado`,`historial`,`fecha`) VALUE(NULL,%s,%s,%s,%s,%s,%s,%s);"
-        datos=(_nombre,_barrio,_calle,_problema,_estado,_historial,_fecha )
+        sql= "INSERT INTO   `reclamos` ( `id`, `nombre`, `barrio`,  `calle`,  `problema`,`estado`,`fecha`) VALUE(NULL,%s,%s,%s,%s,%s,%s);"
+        datos=(_nombre,_barrio,_calle,_problema,_estado,_fecha )
         conexion= mysql.connect()
         cursor=conexion.cursor()
         cursor.execute(sql,datos)
@@ -91,13 +105,50 @@ def guardar():
         reclamo_id = cursor.lastrowid
 
         sql_historial="INSERT INTO `historial` (`id`, `detalles`, `fecha`, `reclamo_id`) VALUE(NULL, %s, %s, %s);"
-        datos_historial=(_historial, _fecha, reclamo_id)
+        datos_historial=(_problema, _fecha, reclamo_id)
         cursor.execute(sql_historial, datos_historial)
         conexion.commit()
         messagebox.showinfo("Exito", "Reclamo agregado correctamente")
 
-    
+
     return redirect('/pendientes')
+
+
+@app.route('/addhistorial/<int:id>', methods=['GET'])
+def verhistorial(id):
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+
+    sql = "SELECT * FROM reclamos WHERE id = %s"
+    cursor.execute(sql, (id,))
+    elemento = cursor.fetchone()
+    historial= elemento[6]
+
+
+    return render_template('pages/addhistorial.html', id=id, historial=historial)
+
+
+@app.route('/addhistorial/guardar/<int:id>', methods=['POST'])
+def addhistorial(id):
+    fecha_actual = date.today()
+    _estado=1
+    _fecha=fecha_actual
+    _reclamo_id=id
+    _historial=request.form['txtHistorial'] + " (Realizado)"
+    sql = "UPDATE `reclamos` SET  `estado` = %s  WHERE `reclamos`.`id` = %s"
+    datos = (_estado, id)
+    conexion = mysql.connect()
+    cursor = conexion.cursor()
+    cursor.execute(sql, datos)
+    conexion.commit()
+    sql_historial="INSERT INTO `historial` (`id`, `detalles`, `fecha`, `reclamo_id`) VALUE(NULL, %s, %s, %s);"
+    datos_historial=(_historial, _fecha, _reclamo_id)
+    cursor.execute(sql_historial, datos_historial)
+    conexion.commit()
+    return redirect('/realizados')
+
+
+
 
 @app.route('/edit/<int:id>', methods=['GET'])
 def editar(id):
@@ -113,11 +164,10 @@ def editar(id):
     barrio = elemento[3]
     problema = elemento[4]
     estado= elemento[5]
-    historial= elemento[6]
-    fecha= elemento[7]
+    fecha= elemento[6]
 
 
-    return render_template('pages/edit.html', id=id, nombre=nombre, calle=calle, barrio=barrio, problema=problema, estado=estado, historial=historial, fecha=fecha)
+    return render_template('pages/edit.html', id=id, nombre=nombre, calle=calle, barrio=barrio, problema=problema, estado=estado, fecha=fecha)
 
 @app.route('/edit/guardar/<int:id>', methods=['POST'])
 def actualizar(id):
@@ -130,15 +180,15 @@ def actualizar(id):
     _estado=0
     _fecha=fecha_actual
     _reclamo_id=id
-    _historial=request.form['txtHistorial']
-    sql = "UPDATE `reclamos` SET `nombre` = %s, `barrio` = %s, `calle` = %s, `problema` = %s, `estado` = %s,`historial` = %s  WHERE `reclamos`.`id` = %s"
-    datos = (_nombre, _barrio, _calle, _problema,_estado,_historial, id)
+    sql = "UPDATE `reclamos` SET `nombre` = %s, `barrio` = %s, `calle` = %s, `problema` = %s, `estado` = %s  WHERE `reclamos`.`id` = %s"
+    datos = (_nombre, _barrio, _calle, _problema,_estado, id)
     conexion = mysql.connect()
     cursor = conexion.cursor()
     cursor.execute(sql, datos)
     conexion.commit()
-    sql_historial="INSERT INTO `historial` (`id`, `detalles`, `fecha`, `reclamo_id`) VALUE(NULL, %s, %s, %s);"
-    datos_historial=(_historial, _fecha, _reclamo_id)
+      # Actualizar el campo "detalles" en la tabla de historial
+    sql_historial = "UPDATE `historial` SET `detalles` = %s ,`fecha` =%s WHERE `reclamo_id` = %s"
+    datos_historial = (_problema,  _fecha,_reclamo_id)
     cursor.execute(sql_historial, datos_historial)
     conexion.commit()
     return redirect('/pendientes')
@@ -207,7 +257,7 @@ def ver_reclamo(id):
     barrio = elemento[3]
     problema = elemento[4]
     estado= elemento[5]
-    fecha= elemento[7]
+    fecha= elemento[6]
 
     conexion=mysql.connect()
     cursor= conexion.cursor()
